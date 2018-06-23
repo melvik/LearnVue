c57439deaece22f25b879baf10204c40f464141f
Hovik Melkomian <melvik@gmail.com>
2018-06-23 00:38:49 -0700

    start firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import AppDate from '@/components/AppDate'
@@ -8,6 +9,17 @@ import store from '@/store'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+  // Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyCpE5ncxcxWmjWzuLjc0ppw3dc-XLglTm4',
+  authDomain: 'hovikvueschoolforum.firebaseapp.com',
+  databaseURL: 'https://hovikvueschoolforum.firebaseio.com',
+  projectId: 'hovikvueschoolforum',
+  storageBucket: '',
+  messagingSenderId: '852367972944'
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
